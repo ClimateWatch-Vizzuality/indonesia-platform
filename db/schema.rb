5bf3181accf4c61440e040deda370fa7e27f73f9
Alvaro Leal <pursuance@gmail.com>
2018-11-06 11:34:40 +0100

    Change schema and application name

--- db/schema.rb
+++ db/schema.rb
@@ -10,85 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_220200) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "historical_emissions_data_sources", force: :cascade do |t|
-    t.text "name"
-    t.text "display_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "historical_emissions_gases", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "historical_emissions_gwps", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "historical_emissions_records", force: :cascade do |t|
-    t.bigint "location_id"
-    t.bigint "data_source_id"
-    t.bigint "sector_id"
-    t.bigint "gas_id"
-    t.bigint "gwp_id"
-    t.jsonb "emissions"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["data_source_id"], name: "index_historical_emissions_records_on_data_source_id"
-    t.index ["gas_id"], name: "index_historical_emissions_records_on_gas_id"
-    t.index ["gwp_id"], name: "index_historical_emissions_records_on_gwp_id"
-    t.index ["location_id"], name: "index_historical_emissions_records_on_location_id"
-    t.index ["sector_id"], name: "index_historical_emissions_records_on_sector_id"
-  end
-
-  create_table "historical_emissions_sectors", force: :cascade do |t|
-    t.bigint "parent_id"
-    t.bigint "data_source_id"
-    t.text "name"
-    t.text "annex_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["data_source_id"], name: "index_historical_emissions_sectors_on_data_source_id"
-    t.index ["parent_id"], name: "index_historical_emissions_sectors_on_parent_id"
-  end
-
-  create_table "location_members", force: :cascade do |t|
-    t.bigint "location_id"
-    t.bigint "member_id"
-    t.index ["location_id"], name: "index_location_members_on_location_id"
-    t.index ["member_id"], name: "index_location_members_on_member_id"
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.text "iso_code3", null: false
-    t.text "iso_code2", null: false
-    t.text "location_type", null: false
-    t.text "wri_standard_name", null: false
-    t.boolean "show_in_cw", default: true, null: false
-    t.text "pik_name"
-    t.text "cait_name"
-    t.text "ndcp_navigators_name"
-    t.text "unfccc_group"
-    t.json "topojson"
-    t.jsonb "centroid"
-  end
-
-  add_foreign_key "historical_emissions_records", "historical_emissions_data_sources", column: "data_source_id", on_delete: :cascade
-  add_foreign_key "historical_emissions_records", "historical_emissions_gases", column: "gas_id", on_delete: :cascade
-  add_foreign_key "historical_emissions_records", "historical_emissions_gwps", column: "gwp_id", on_delete: :cascade
-  add_foreign_key "historical_emissions_records", "historical_emissions_sectors", column: "sector_id", on_delete: :cascade
-  add_foreign_key "historical_emissions_records", "locations", on_delete: :cascade
-  add_foreign_key "historical_emissions_sectors", "historical_emissions_data_sources", column: "data_source_id", on_delete: :cascade
-  add_foreign_key "historical_emissions_sectors", "historical_emissions_sectors", column: "parent_id", on_delete: :cascade
-  add_foreign_key "location_members", "locations", column: "member_id", on_delete: :cascade
-  add_foreign_key "location_members", "locations", on_delete: :cascade
 end
