ea7a41b2c833749fc7fab1133d8af114134f59d4
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-11-07 16:40:12 +0100

    Add first part of the footer

--- app/javascript/app/components/footer/footer-component.jsx
+++ app/javascript/app/components/footer/footer-component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BottomBar, Icon } from 'cw-components';
 import resourceWatchLogo from 'assets/resource-watch-logo.svg';
+import contactIcon from 'assets/contact.svg';
+import cx from 'classnames';
+import founders from 'constants/founders';
 import styles from './footer-styles.scss';
 
 const rwLogo = (
@@ -11,9 +14,43 @@ const rwLogo = (
   />
 );
 
+const contactLinkIcon = (
+  <Icon alt="Contact Link" icon={contactIcon} theme={{ icon: styles.icon }} />
+);
+
 function Footer() {
   return (
     <footer className={styles.footer}>
+      <div className={styles.line} />
+      <div className={styles.grid}>
+        <div className={styles.content}>
+          <span className={styles.text}>
+            Funding for this initiative is provided by
+          </span>
+          {founders.map(
+            partner => partner.img && (
+            <div key={partner.img.alt} className={styles.logoContainer}>
+              <a
+                className={cx(styles.logo, styles[partner.img.customClass])}
+                href={partner.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className={styles.defaultLogo}
+                  src={partner.img.src}
+                  alt={partner.img.alt}
+                />
+              </a>
+            </div>
+              )
+          )}
+        </div>
+        <a className={styles.contactUs} href="mailto:climatewatch@wri.org">
+          {contactLinkIcon}
+          CONTACT US
+        </a>
+      </div>
       <BottomBar
         className={styles.content}
         theme={{
---
+++ app/javascript/app/constants/founders.js
@@ -0,0 +1,10 @@
+import fmencbnsImage from 'assets/fmencbns';
+
+export default [
+  {
+    link: 'http://www.bmub.bund.de/en/',
+    orderingString: 'FMENCBNS',
+    img: { alt: 'FMENCBNS', src: fmencbnsImage, customClass: 'BMULogo' },
+    description: 'This initiative is a part of the International Climate Initiative (IKI).'
+  }
+];
