0027254d49d517c5f67a389021023f5fe4b215d3
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-11-07 14:55:36 +0100

    Fix footer styles

--- app/javascript/app/components/footer/footer-component.jsx
+++ app/javascript/app/components/footer/footer-component.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import { BottomBar } from 'cw-components';
-
+import { BottomBar, Icon } from 'cw-components';
+import resourceWatchLogo from 'assets/resource-watch-logo.svg';
 import styles from './footer-styles.scss';
+
+const rwLogo = (
+  <Icon
+    alt="Resource Watch Logo"
+    icon={resourceWatchLogo}
+    theme={{ icon: styles.icon }}
+  />
+);
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <BottomBar
-        footerText={
-          `Powered by <span style="text-transform: uppercase"><b>Climate</b>Watch</span>`
-        }
         className={styles.content}
         theme={{
           bottomBar: styles.bottomBar,
           bottomBarContainer: styles.bottomBarContainer,
           bottomBarText: styles.bottomBarText
         }}
-      />
+      >
+        Climate Watch © {new Date().getFullYear()} Powered by
+        <a
+          href="https://resourcewatch.org/"
+          alt="Resource Watch Link"
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {rwLogo}
+          <span className={styles.resourceWatch}>
+            <span className={styles.resource}>
+              RESOURCE
+            </span>
+            WATCH
+          </span>
+        </a>
+      </BottomBar>
     </footer>
   );
 }
-
 export default Footer;
