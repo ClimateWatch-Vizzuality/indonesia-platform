be5645eb5351c86db4670d30ca2bd38c40b8fcfb
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-11-07 18:19:41 +0100

    Add permissions & licensing link (fake href for now)

--- app/javascript/app/components/footer/footer-component.jsx
+++ app/javascript/app/components/footer/footer-component.jsx
@@ -18,64 +18,87 @@ const contactLinkIcon = (
   <Icon alt="Contact Link" icon={contactIcon} theme={{ icon: styles.icon }} />
 );
 
-function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.line} />
-      <div className={styles.grid}>
-        <div className={styles.content}>
-          <span className={styles.text}>
-            Funding for this initiative is provided by
-          </span>
-          {founders.map(
-            partner => partner.img && (
-            <div key={partner.img.alt} className={styles.logoContainer}>
-              <a
-                className={cx(styles.logo, styles[partner.img.customClass])}
-                href={partner.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  className={styles.defaultLogo}
-                  src={partner.img.src}
-                  alt={partner.img.alt}
-                />
-              </a>
-            </div>
-              )
-          )}
-        </div>
-        <a className={styles.contactUs} href="mailto:climatewatch@wri.org">
-          {contactLinkIcon}
-          CONTACT US
-        </a>
+const upperFooter = (
+  <React.Fragment>
+    <div className={styles.line} />
+    <div className={styles.grid}>
+      <div className={styles.content}>
+        <span className={styles.text}>
+          Funding for this initiative is provided by
+        </span>
+        {founders.map(
+          partner => partner.img && (
+          <div key={partner.img.alt} className={styles.logoContainer}>
+            <a
+              className={cx(styles.logo, styles[partner.img.customClass])}
+              href={partner.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={styles.defaultLogo}
+                src={partner.img.src}
+                alt={partner.img.alt}
+              />
+            </a>
+          </div>
+            )
+        )}
       </div>
-      <BottomBar
-        className={styles.content}
-        theme={{
-          bottomBar: styles.bottomBar,
-          bottomBarContainer: styles.bottomBarContainer,
-          bottomBarText: styles.bottomBarText
-        }}
-      >
-        Climate Watch © {new Date().getFullYear()} Powered by
+      <a className={styles.contactUs} href="mailto:climatewatch@wri.org">
+        {contactLinkIcon}
+        CONTACT US
+      </a>
+    </div>
+  </React.Fragment>
+);
+
+const bottomBar = (
+  <React.Fragment>
+    <BottomBar
+      className={styles.content}
+      theme={{
+        bottomBar: styles.bottomBar,
+        bottomBarContainer: styles.bottomBarContainer,
+        bottomBarText: styles.bottomBarText
+      }}
+    >
+      <div className={styles.barContent}>
         <a
-          href="https://resourcewatch.org/"
-          alt="Resource Watch Link"
           className={styles.link}
+          href="permissions-and-licensing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          {rwLogo}
-          <span className={styles.resourceWatch}>
-            <span className={styles.resource}>
-              RESOURCE
-            </span>
-            WATCH
-          </span>
+          Permissions & Licensing
         </a>
-      </BottomBar>
+        <div className={styles.copyright}>
+          Climate Watch © {new Date().getFullYear()} Powered by
+          <a
+            href="https://resourcewatch.org/"
+            alt="Resource Watch Link"
+            className={styles.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {rwLogo}
+            <span className={styles.resourceWatch}>
+              <span className={styles.resource}>
+                RESOURCE
+              </span>
+              WATCH
+            </span>
+          </a>
+        </div>
+      </div>
+    </BottomBar>
+  </React.Fragment>
+);
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      {upperFooter}
+      {bottomBar}
     </footer>
   );
 }
