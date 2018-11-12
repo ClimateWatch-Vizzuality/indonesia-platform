a9f5606ba74635a58c057c91f8a8fa0d6691dce6
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-11-12 11:49:34 +0000

    Finish homepage subsections

--- app/javascript/app/components/stories/stories-component.jsx
+++ app/javascript/app/components/stories/stories-component.jsx
@@ -1,10 +1,18 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-// import cx from 'classnames';
+import cx from 'classnames';
 import SectionTitle from 'components/section-title';
+import { Icon, Button } from 'cw-components';
+import yellowWriLogo from 'assets/yellow-wri-logo.svg';
+import { WRI_WEBSITE } from 'constants/links';
+import button from 'styles/themes/button';
 import styles from './stories-styles.scss';
 
 class Stories extends PureComponent {
+  handleBtnClick = () => {
+    window.open(WRI_WEBSITE, '_blank');
+  };
+
   render() {
     const { stories } = this.props;
 
@@ -19,12 +27,35 @@ class Stories extends PureComponent {
               href={story.link}
               target="_blank"
               rel="noopener noreferrer"
-              /* onClick={() => handleClickAnalytics(story.title)} */
             >
-              {story.title}
+              <div className={styles.storyDate}>
+                {story.date}
+              </div>
+              <div className={styles.storyTitle}>
+                {story.title}
+              </div>
+              <div className={styles.storyDescription}>
+                {story.description}
+              </div>
+              <div className={styles.logoContainer}>
+                <Icon
+                  alt="Wri logo"
+                  icon={yellowWriLogo}
+                  theme={{ icon: styles.icon }}
+                />
+                <span className={styles.text}>
+                  By World Resources Institute
+                </span>
+              </div>
             </a>
           ))}
         </div>
+        <Button
+          onClick={this.handleBtnClick}
+          theme={{ button: cx(button.primary, styles.button) }}
+        >
+          More Stories
+        </Button>
       </div>
     );
   }
--- app/javascript/app/components/stories/stories.js
+++ app/javascript/app/components/stories/stories.js
@@ -2,9 +2,27 @@ import { connect } from 'react-redux';
 import Component from './stories-component';
 
 const stories = [
-  { link: 'bla', background_image_url: 'assets/province-image.png' },
-  { link: 'ble', background_image_url: 'assets/province-image.png' },
-  { link: 'bli', background_image_url: 'assets/province-image.png' }
+  {
+    date: 'Oct 5, 2018',
+    title: 'How can Indonesia achieve its climate change mitigation goal?',
+    description: 'An analysis of potential emissions reductions from energy and land-use policies.',
+    link: 'todo',
+    background_image_url: 'assets/story-1.png'
+  },
+  {
+    date: 'Oct 5, 2018',
+    title: 'Got climate questions?',
+    description: 'Climate Watch has answers!',
+    link: 'todo',
+    background_image_url: 'assets/story-2.png'
+  },
+  {
+    date: 'Oct 5, 2018',
+    title: 'Stepping into the future',
+    description: 'Creating long-term strategies for low GHG emissions and development.',
+    link: 'todo',
+    background_image_url: 'assets/story-3.png'
+  }
 ];
 
 const mapStateToProps = () => ({ stories });
