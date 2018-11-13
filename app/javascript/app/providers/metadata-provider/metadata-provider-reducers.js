9740088084b9aef134041dab9b4b56d17ca4ef06
Alvaro Leal <pursuance@gmail.com>
2018-11-13 17:19:00 +0100

    Get reducers from South Africa and tweak them

---
+++ app/javascript/app/providers/metadata-provider/metadata-provider-reducers.js
@@ -0,0 +1,75 @@
+import camelCase from 'lodash/camelCase';
+import sortBy from 'lodash/sortBy';
+import * as actions from './metadata-provider-actions';
+
+export const initialState = {
+  ghg: { loading: false, loaded: false, error: false, data: null }
+};
+
+function parseDataByMeta(data, meta) {
+  switch (meta) {
+    case 'ghg': {
+      const dataParsed = {};
+      Object.keys(data).forEach(
+        key => {
+          const camelCasedkey = camelCase(key);
+          dataParsed[camelCasedkey] = sortBy(
+            data[key].map(item => {
+              let newItem = {
+                value: item.id,
+                label: key === 'location'
+                  ? item.wri_standard_name.trim()
+                  : item.name.trim(),
+                ...(item.parent_id && { parentId: item.parent_id })
+              };
+              if (key === 'location') {
+                newItem = { ...newItem, iso: item.iso_code_3 };
+              }
+              if (key === 'dataSource') {
+                newItem = {
+                  ...newItem,
+                  location: item.location_ids,
+                  sector: item.sector_ids,
+                  gas: item.gas_ids,
+                  gwp: item.gwp_ids,
+                  source: item.source.replace('historical_emissions_', '')
+                };
+              }
+              return newItem;
+            }),
+            'label'
+          );
+        },
+        this
+      );
+
+      return dataParsed;
+    }
+
+    default:
+      return data;
+  }
+}
+
+export default {
+  [actions.fetchMetaInit]: (state, { payload }) => ({
+    ...state,
+    [payload.meta]: { ...state[payload.meta], loading: true }
+  }),
+  [actions.fetchMetaReady]: (state, { payload }) => ({
+    ...state,
+    [payload.meta]: {
+      ...state[payload.meta],
+      loading: false,
+      data: parseDataByMeta(payload.data, payload.meta)
+    }
+  }),
+  [actions.fetchMetaFail]: (state, { payload }) => ({
+    ...state,
+    [payload.meta]: {
+      ...state[payload.meta],
+      loading: false,
+      error: payload.error
+    }
+  })
+};
