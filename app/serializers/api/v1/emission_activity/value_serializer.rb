feb0a0de8405da1071accf262a8d3644e99c2ba7
Tomasz Subik <tsubik@gmail.com>
2018-11-13 15:21:43 +0100

    emission activity: iso_code3

--- app/serializers/api/v1/emission_activity/value_serializer.rb
+++ app/serializers/api/v1/emission_activity/value_serializer.rb
@@ -3,11 +3,16 @@ module Api
     module EmissionActivity
       class ValueSerializer < ActiveModel::Serializer
         attribute :location
+        attribute :location_iso_code3
         attribute :sector
         attribute :activity
         attribute :emissions
 
         def location
+          object.location.wri_standard_name
+        end
+
+        def location_iso_code3
           object.location.iso_code3
         end
 
