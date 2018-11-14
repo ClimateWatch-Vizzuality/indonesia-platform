6882b03b9bb893ea3987620b235503effe9c5243
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-11-14 17:29:26 +0000

    If no locale provided in query param return english translations

--- app/controllers/api/v1/section_content_controller.rb
+++ app/controllers/api/v1/section_content_controller.rb
@@ -1,9 +1,16 @@
 module Api
   module V1
     class SectionContentController < ApiController
+      DEFAULT_LANGUAGE = 'en'.freeze
+
       def index
         locale = params[:locale]
-        section_contents = locale ? SectionContent.where(locale: locale) : SectionContent.all
+        section_contents =
+          if locale
+            SectionContent.where(locale: locale)
+          else
+            SectionContent.where(locale: DEFAULT_LANGUAGE)
+          end
 
         respond_to do |format|
           format.json do
