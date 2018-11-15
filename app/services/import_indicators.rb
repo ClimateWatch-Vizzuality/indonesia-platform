aeb4f15051a81e1dcafe8a13512b8f20ad4bdce8
Tomasz Subik <tsubik@gmail.com>
2018-11-15 20:23:56 +0100

    indicators: importer

---
+++ app/services/import_indicators.rb
@@ -0,0 +1,64 @@
+class ImportIndicators
+  INDICATORS_FILEPATH = "#{CW_FILES_PREFIX}indicators/indicators.csv".freeze
+
+  INDICATOR_VALUE_FILEPATHS = %W(
+    #{CW_FILES_PREFIX}indicators/socioeconomics.csv
+    #{CW_FILES_PREFIX}indicators/pc_forest.csv
+    #{CW_FILES_PREFIX}indicators/pc_agriculture.csv
+    #{CW_FILES_PREFIX}indicators/pc_energy.csv
+    #{CW_FILES_PREFIX}indicators/vulnerability_adaptivity.csv
+  ).freeze
+
+  def call
+    cleanup
+
+    import_indicators(S3CSVReader.read(INDICATORS_FILEPATH))
+
+    INDICATOR_VALUE_FILEPATHS.each do |filepath|
+      import_indicator_values(S3CSVReader.read(filepath))
+    end
+  end
+
+  private
+
+  def cleanup
+    Indicator.delete_all
+    IndicatorValue.delete_all
+  end
+
+  def import_indicator_values(csv)
+    csv.each do |row|
+      begin
+        IndicatorValue.create!(
+          location: Location.find_by(iso_code3: row[:geoid]),
+          indicator: Indicator.find_by(code: row[:ind_code]),
+          category: row[:category],
+          values: values(row)
+        )
+      rescue ActiveRecord::RecordInvalid => invalid
+        STDERR.puts "Error importing #{row.to_s.chomp}: #{invalid}"
+      end
+    end
+  end
+
+  def import_indicators(csv)
+    csv.each do |row|
+      begin
+        Indicator.create!(
+          section: row[:section],
+          code: row[:ind_code],
+          name: row[:indicator],
+          unit: row[:unit]
+        )
+      rescue ActiveRecord::RecordInvalid => invalid
+        STDERR.puts "Error importing #{row.to_s.chomp}: #{invalid}"
+      end
+    end
+  end
+
+  def values(row)
+    row.headers.grep(/\d{4}/).map do |year|
+      {year: year.to_s, value: row[year]&.delete('%', ',')&.to_f}
+    end
+  end
+end
---
+++ spec/services/import_indicators_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+object_contents = {
+  "#{CW_FILES_PREFIX}indicators/indicators.csv" => <<~END_OF_CSV,
+    section,ind_code,indicator,unit
+    socioeconomic,pop_total,Population,thousand
+    forestry,tree_cover_lost,Tree cover lost,ha
+    agriculture,area_harvested,Harvested Area of Paddy,ha
+    energy,capacity,Installed capacity,MW
+    adaptation,Adap_1,Mineral water sources,index
+  END_OF_CSV
+  "#{CW_FILES_PREFIX}indicators/socioeconomics.csv" => <<~END_OF_CSV,
+    geoid,source,ind_code,category,2010,2011
+    IDN,STATIDNa,pop_total,238500,242000
+  END_OF_CSV
+  "#{CW_FILES_PREFIX}indicators/pc_forest.csv" => <<~END_OF_CSV,
+    source,geoid,ind_code,category,"1990 -1996","1996 - 2000"
+    ,ID.AC,tree_cover_lost,non peat,,303973.8169
+  END_OF_CSV
+  "#{CW_FILES_PREFIX}indicators/pc_agriculture.csv" => <<~END_OF_CSV,
+    source,geoid,ind_code,2010,2011,2012
+    STATIDNc,ID.AC,area_harvested,"352,281","380,686","387,803"
+  END_OF_CSV
+  "#{CW_FILES_PREFIX}indicators/pc_energy.csv" => <<~END_OF_CSV,
+    source,geoid,ind_code,category,2011,2012,2013
+    STATEL,ID.BA,capacity,diesel power plant,79.3,79.4,79.55
+  END_OF_CSV
+  "#{CW_FILES_PREFIX}indicators/vulnerability_adaptivity.csv" => <<~END_OF_CSV,
+    source,geoid,ind_code,2011,2014
+    SIDIK,ID.AC,Adap_1,0.8,0.8
+  END_OF_CSV
+}
+
+RSpec.describe ImportIndicators do
+  subject { ImportIndicators.new.call }
+
+  before :all do
+    Aws.config[:s3] = {
+      stub_responses: {
+        get_object: lambda do |context|
+          {body: object_contents[context.params[:key]]}
+        end
+      }
+    }
+  end
+
+  before :each do
+    FactoryBot.create(:location, iso_code3: 'ID.AC')
+    FactoryBot.create(:location, iso_code3: 'ID.BA')
+    FactoryBot.create(:location, iso_code3: 'IDN')
+  end
+
+  after :all do
+    Aws.config[:s3] = {
+      stub_responses: nil
+    }
+  end
+
+  it 'Creates new indicators' do
+    expect { subject }.to change { Indicator.count }.by(5)
+  end
+
+  it 'Creates new indicator values' do
+    expect { subject }.to change { IndicatorValue.count }.by(5)
+  end
+end
