5d46d22f8cba2f420b5ae5ad513c0956d3983c9e
Javi Abia <weberjavi@gmail.com>
2018-11-15 12:48:32 +0100

    Update carousel to match new version

--- app/javascript/app/pages/home/sections-slideshow/sections-slideshow-component.jsx
+++ app/javascript/app/pages/home/sections-slideshow/sections-slideshow-component.jsx
@@ -12,30 +12,37 @@ const pagingTitles = [
   'Top 10 emitting provinces'
 ];
 
-const CustomSlide = ({ title }) => (
+const TopSlide = ({ title }) => (
   <div className={styles.slideWrapper}>
     <h3 className={styles.slideTitle}>{title}</h3>
     <p className={styles.slideParagraph}>
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
     </p>
     <Button theme={{ button: styles.button }}>Go to GHG target</Button>
-    <div>
-      <img className={styles.image} src={ghgImage} alt="ghg" />
-    </div>
+  </div>
+);
+
+const BottomSlide = () => (
+  <div className={styles.bottomSlideContainer}>
+    <img className={styles.image} src={ghgImage} alt="ghg" />
   </div>
 );
 
 const SectionsSlideshowComponent = () => (
   <section className={styles.container}>
     <Carousel pagingTitles={pagingTitles}>
-      <CustomSlide title="Greenhouse Gas Emissions and Targets" />
-      <CustomSlide title="National context" />
-      <CustomSlide title="Annual emissions" />
-      <CustomSlide title="Top 10 emitting provinces" />
+      <TopSlide title="Greenhouse Gas Emissions and Targets" topSlide />
+      <BottomSlide bottomSlide />
+      <TopSlide title="National context" topSlide />
+      <BottomSlide bottomSlide />
+      <TopSlide title="Annual emissions" topSlide />
+      <BottomSlide bottomSlide />
+      <TopSlide title="Top 10 emitting provinces" topSlide />
+      <BottomSlide bottomSlide />
     </Carousel>
   </section>
 );
 
-CustomSlide.propTypes = { title: PropTypes.string.isRequired };
+TopSlide.propTypes = { title: PropTypes.string.isRequired };
 
 export default SectionsSlideshowComponent;
