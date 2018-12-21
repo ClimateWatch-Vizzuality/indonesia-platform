8bd8c5d724815ed834221560c3b860e0652b1028
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-12-21 17:56:48 +0000

    If province section already chosen upon changing province dont clear the section

--- app/javascript/app/components/regions-select/regions-select-selectors.js
+++ app/javascript/app/components/regions-select/regions-select-selectors.js
@@ -1,17 +1,35 @@
 import { createStructuredSelector, createSelector } from 'reselect';
 import isEmpty from 'lodash/isEmpty';
+import get from 'lodash/get';
+
+const DEFAULT_SECTION = 'regions-ghg-emissions';
 
 const getProvinces = ({ provinces }) => provinces && provinces.data || null;
+const getLocation = ({ location }) => location && location;
 
-const getParsedProvinces = createSelector(getProvinces, provinces => {
-  if (!provinces || isEmpty(provinces)) return [];
+const getSection = createSelector(getLocation, location => {
+  if (!location) return null;
 
-  return provinces.map(province => ({
-    value: province.iso_code3,
-    label: province.wri_standard_name,
-    path: `/regions/${province.iso_code3}/regions-ghg-emissions`
-  }));
+  return get(location, 'payload.region')
+    ? get(location, 'payload.section')
+    : '';
 });
+
+const getParsedProvinces = createSelector([ getProvinces, getSection ], (
+  provinces,
+  section
+) =>
+  {
+    if (!provinces || isEmpty(provinces)) return [];
+
+    const provinceSection = section || DEFAULT_SECTION;
+
+    return provinces.map(province => ({
+      value: province.iso_code3,
+      label: province.wri_standard_name,
+      path: `/regions/${province.iso_code3}/${provinceSection}`
+    }));
+  });
 
 export const getProvincesData = createStructuredSelector({
   provinces: getParsedProvinces
