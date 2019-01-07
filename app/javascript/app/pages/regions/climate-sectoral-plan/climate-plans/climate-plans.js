87e83d69cc62586e3130b6042fe3a4cd68f8b184
mgajowczyk <gajowczyk.marta@gmail.com>
2019-01-07 11:11:29 +0000

    Wire up climate sectoral components with translations

--- app/javascript/app/pages/regions/climate-sectoral-plan/climate-plans/climate-plans.js
+++ app/javascript/app/pages/regions/climate-sectoral-plan/climate-plans/climate-plans.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux';
+import withTranslations from 'providers/translations-provider/with-translations.hoc';
 import Component from './climate-plans-component';
 import { getClimatePlans } from './climate-plans-selectors';
 
 const mapStateToProps = getClimatePlans;
 
-export default connect(mapStateToProps, null)(Component);
+export default connect(mapStateToProps, null)(withTranslations(Component));
