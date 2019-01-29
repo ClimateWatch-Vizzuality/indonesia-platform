55254e2023c0c94310b69d5622b2dd750fd04581
Tomasz Subik <tsubik@gmail.com>
2019-01-29 12:40:09 +0100

    move locale config to locale.rb

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,11 +1,15 @@
 require 'i18n/backend/active_record'
 
+I18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
 I18n.available_locales = [:en, :id]
 I18n.default_locale = :en
+I18n.fallbacks = { :id => :en }
 
 module I18n
   class << self
     def all(key_prefix)
+      return t(key_prefix) unless defined? fallbacks
+
       fallbacks[I18n.locale].reverse.reduce({}) do |translations, fallback|
         translations.deep_merge!(backend.translate(fallback, key_prefix))
       end
