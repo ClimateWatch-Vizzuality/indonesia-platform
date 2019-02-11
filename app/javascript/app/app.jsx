6fa8bd0d045b5b2e1d143796ac5fce3fbdb58240
Tomasz Subik <tsubik@gmail.com>
2019-02-11 19:25:37 +0100

    webpack: allow to easily test locally linked cw-components

--- app/javascript/app/app.jsx
+++ app/javascript/app/app.jsx
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import 'whatwg-fetch';
-import 'cw-components/dist/main';
 
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -8,6 +7,16 @@ import { Provider } from 'react-redux';
 
 import store from 'app/store';
 import Root from './layouts/root';
+
+// using try catch to get warning if module not found
+// module will not be found if cw-components linked locally using yarn link
+// but we don't have to import in this case
+try {
+  // eslint-disable-next-line global-require
+  require('cw-components/dist/main');
+} catch (err) {
+  console.warn('cw-components/dist/main not found');
+}
 
 const App = ({ data }) => (
   <Provider store={store(data)}>
