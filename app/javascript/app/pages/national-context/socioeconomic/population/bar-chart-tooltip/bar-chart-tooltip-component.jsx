6e851359dd92ec67d9c1f457a755dc157455f58a
Javi Abia <weberjavi@gmail.com>
2019-03-06 13:10:53 +0100

    Update tooltip

--- app/javascript/app/pages/national-context/socioeconomic/population/bar-chart-tooltip/bar-chart-tooltip-component.jsx
+++ app/javascript/app/pages/national-context/socioeconomic/population/bar-chart-tooltip/bar-chart-tooltip-component.jsx
@@ -1,37 +1,61 @@
 import React, { PureComponent } from 'react';
 import Proptypes from 'prop-types';
+import get from 'lodash/get';
 import styles from './bar-chart-tooltip-styles.scss';
+
+const renderValue = ({ formatFunction }, value) => {
+  if (!value) return 'n/a';
+  if (formatFunction) return formatFunction(value);
+  return value;
+};
 
 class CustomTooltip extends PureComponent {
   render() {
     const { content, config } = this.props;
     const hasContent = content && content.payload && content.payload.length > 0;
-    const payload = content &&
-      content.payload &&
-      content.payload[0] &&
-      content.payload[0].payload;
+    const payload = get(content, 'payload[0].payload');
     const tooltipConfig = config && config.tooltip;
-    const yValueFormatFunction = tooltipConfig && tooltipConfig.y.format;
+    const yKeys = payload && Object
+        .keys(payload)
+        .filter(k => k !== 'x')
+        .sort((a, b) => payload[b] - payload[a]);
     return (
       <div className={styles.tooltip}>
         {
           hasContent ? (
             <div>
               <div className={styles.tooltipHeader}>
-                <span className={styles.title}>
-                  {payload.x} {tooltipConfig.indicator}
+                <span>
+                  {payload.x}
+                </span>
+                <span>
+                  {tooltipConfig.indicator}
                 </span>
               </div>
               <div className={styles.content}>
-                <span>{tooltipConfig.y.label}</span>
-                {' '}
-                <span>
-                  {
-                    yValueFormatFunction
-                      ? yValueFormatFunction(payload.y)
-                      : tooltipConfig.y.format
-                  }
-                </span>
+                {
+                  yKeys && yKeys.map(k => (
+                    <div className={styles.yDataContainer}>
+                      {
+                          tooltipConfig.theme &&
+                            (
+                              <span
+                                style={{
+                                  backgroundColor: tooltipConfig.theme[k].fill
+                                }}
+                                className={styles.dot}
+                              />
+                            )
+                        }
+                      <span className={styles.yLabel}>
+                        {tooltipConfig[k].label}
+                      </span>
+                      <span className={styles.value}>
+                        {renderValue(tooltipConfig, payload[k])}
+                      </span>
+                    </div>
+                    ))
+                }
               </div>
             </div>
 ) : <div>No data</div>
