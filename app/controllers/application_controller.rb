bb6dfff350f823404cacfe36946e6a012ae370f0
Simao Belchior <simaobelchior@gmail.com>
2019-03-12 11:13:14 +0000

    Adds http_basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  unless Rails.env.development?
+    http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'],
+      password: ENV['HTTP_AUTH_PASSWORD']
+  end
+
   def index
     @is_production = Rails.env.production?
   end
